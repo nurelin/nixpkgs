d83b0ece7f57f53dacd7365c7c134ff90005404f
worldofpeace <worldofpeace@protonmail.ch>
2019-09-27 21:06:32 -0400

    squash! gnome3.gnome-mahjongg: 3.32.0 → 3.33.90
    gnome3.gnome-mahjongg: 3.32.0 → 3.34.0

--- pkgs/desktops/gnome-3/games/gnome-mahjongg/default.nix
+++ pkgs/desktops/gnome-3/games/gnome-mahjongg/default.nix
@@ -5,11 +5,11 @@
 
 stdenv.mkDerivation rec {
   pname = "gnome-mahjongg";
-  version = "3.33.90";
+  version = "3.34.0";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-mahjongg/${stdenv.lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "07gk4r2mq7znxmriz7irvxh6iih47hgpx38arm31d7r1x2n84m0l";
+    sha256 = "0qyi3jkg112jysrsy5p330b7b92sl09mr8zsimq06q68kj1wzkz5";
   };
 
   passthru = {
